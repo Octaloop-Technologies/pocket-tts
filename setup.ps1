7b8875bae6e8b8988b05f9b0c08b67cd022a0612
Ali Hussain Abid <alihussainabid.octaloop@gmail.com>
2026-07-22 14:02:39 +0500

    Explicit Method naming to avoid any System Alias conflicts.

--- setup.ps1
+++ setup.ps1
@@ -1,15 +1,15 @@
-echo "Make Sure you have cuda 11.8 installed system wide or in your environment."
+Write-Output "Make Sure you have cuda 11.8 installed system wide or in your environment."
 
-echo "Creating Conda Environment with python 3.12"
+Write-Output "Creating Conda Environment with python 3.12"
 conda create --name pocket-tts python=3.12;
 
-echo "Activating conda environment 'pocket-tts'"
+Write-Output "Activating conda environment 'pocket-tts'"
 conda activate pocket-tts;
 
-echo "Upgrading pip"
+Write-Output "Upgrading pip"
 python -m pip install --upgrade pip
 
-echo "installing pytorch compatible with cuda 11.8"
+Write-Output "installing pytorch compatible with cuda 11.8"
 pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu118
 
 pip install -e .
